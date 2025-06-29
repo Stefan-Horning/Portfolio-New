42f124e1f04bcc28efc9c0629a9b91f65d1d11ab
Stefan Horning <stefanhorning02704@gmail.com>
2025-06-29 19:57:30 +0200

    nav & background

---
+++ src/app/homepage/nav/nav.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  imports: [RouterLink,RouterLinkActive,CommonModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
