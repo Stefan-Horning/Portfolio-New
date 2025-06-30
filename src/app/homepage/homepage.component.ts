edc25952c1a80b6f1a919f0b198df39b4c5a483f
Stefan Horning <stefanhorning02704@gmail.com>
2025-06-30 17:37:39 +0200

    create componet

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavComponent,WelcomeComponent],
+  imports: [NavComponent,WelcomeComponent,AboutComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
