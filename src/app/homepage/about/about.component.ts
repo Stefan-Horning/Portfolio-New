0c0ad32662474ef3e22f02a8031e6b624539475f
Stefan Horning <stefanhorning02704@gmail.com>
2025-07-01 22:55:16 +0200

    about me section tech stack & CV

--- src/app/homepage/about/about.component.ts
+++ src/app/homepage/about/about.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  technologies1 = [
+    'Angular',
+    'Ts',
+    'HTML',
+  ];
+  technologies2 = [
+    '',
+    'Git',
+    'Python',
+    'Django',
+  ];
 }
