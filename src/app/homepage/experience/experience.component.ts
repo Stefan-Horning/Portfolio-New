b7533e7c98588eb49b0911dae03c61bcbfd20404
Stefan Horning <stefanhorning02704@gmail.com>
2025-07-12 21:30:59 +0200

    experience

--- src/app/homepage/experience/experience.component.ts
+++ src/app/homepage/experience/experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Experience } from '../../interface/expereince.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
@@ -11,39 +12,39 @@ export class experienceComponent {
   experiences: Experience[] = [
     {
       company: 'Berufskolleg Uerdingen',
-      companyLogo: 'bk.png',
-      title: 'High School Diploma – Focus on Mathematics & Computer Science',
+      companyLogo: 'assets/logos/bku.png',
+      title: 'Abitur – Mathematics & Computer Science',
       startDate: '2023',
-       endDate: '2026',
+      endDate: '2026',
       description: 'Currently pursuing my German Abitur with a specialization in Math and Computer Science.',
       icon: 'assets/icons/school.svg'
     },
     {
       company: 'Grant Thornton AG',
-      companyLogo: 'assets/icons/GT.png',
+      companyLogo: 'assets/logos/GT.png',
       title: 'Cybersecurity Intern',
-      startDate: '2025-06-25',
-      endDate: '2025-08-22',
+      startDate: '25.06.2025',
+      endDate: '22.08.2025',
       description: 'Currently completing an internship in cybersecurity, focusing on IT forensics, security guidelines, and risk assessment. Gaining hands-on experience in analyzing security incidents, understanding regulatory compliance, and supporting the development of internal IT security policies.',
       icon: 'assets/icons/IT-Sicherheit.svg'
     },
     {
       company: 'NextLevel GmbH',
-      companyLogo: 'assets/icons/freelance.png',
+      companyLogo: 'assets/logos/Nextlevels.png',
       title: 'Frontend Developer intern',
-      startDate: '2024-6-8',
-      endDate: '2024-11-01',
+      startDate: '08.06.2024',
+      endDate: '01.11.2024',
       description: 'Developed Angular-based web applications and gained hands-on experience in building dynamic and modular user interfaces.',
       icon: 'assets/icons/Frontend.svg'
     },
     {
       company: 'Developer Akademie',
-      companyLogo: 'assets/icons/DA.png', 
+      companyLogo: 'assets/logos/DA.png', 
       title: 'Frontend Developer student ',
-      startDate: '2022-11',
-      endDate: '2023-02',
+      startDate: '11.2021',
+      endDate: '02.2023',
       description: 'Completed a practical training program focused on Angular, TypeScript, and UI/UX design principles.',
-      icon: 'assets/icons/internship.svg'
+      icon: 'assets/icons/akademie.svg'
     }
   ];
 
