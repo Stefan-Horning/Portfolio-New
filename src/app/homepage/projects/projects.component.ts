8a2549131b196737c787f109e0a7cb0e42bce140
Stefan Horning <stefanhorning02704@gmail.com>
2025-07-19 22:19:59 +0200

    more resp

--- src/app/homepage/projects/projects.component.ts
+++ src/app/homepage/projects/projects.component.ts
@@ -14,28 +14,28 @@ export class ProjectsComponent {
   projects: Project[] = [
     {
       title: 'DABubble',
-      description: 'The task management app streamlines collaboration by making it easy to create, assign, and track tasks.',
+      description: 'A collaborative tool that makes it easy to create, assign, and track tasks in real-time. Designed to boost productivity and streamline team workflows.',
       imageUrl: '/assets/projects/DABubble.png',
       link: 'https://dabubble.stefan-horning.com/',
       technologies: ['Angular', 'Ts', 'CSS', 'HTML', 'firebase'],
     },
     {
       title: 'Osteomedica',
-      description: 'This live project delivers cutting-edge digital experiences through an iterative process, powered by a PHP-based CMS that ensures flexibility, creativity, and ongoing improvement.',
+      description: 'A live digital platform built on a PHP-based CMS. It supports flexible development and is shaped through continuous iteration, feedback, and creativity.',
       imageUrl: '/assets/projects/osteo.png',
       link: 'https://osteomedica-toenisvorst.de/',
       technologies: ['Angular', 'Ts','HTML','SQL', 'php'],
     },
     {
       title: 'Makeupartist',
-      description: 'This is a live project that combines design and coding to create functional and visually appealing digital solutions. It’s an evolving effort, continuously refined through feedback and new ideas.',
+      description: 'A hands-on project that blends design and development to create functional digital solutions. Continuously refined with input and new ideas.',
       imageUrl: '/assets/projects/makeup.png',
       link: 'https://www.swetlana-makeupartist.de/',
       technologies: ['Angular', 'Ts', 'CSS', 'HTML'],
     },
     {
       title: 'El Pollo Loco',
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and poison bottles to fight against the killer chicken.',
+      description: 'A simple, object-oriented game where you help Pepe collect coins and poison bottles to defeat the menacing killer chicken in a playful adventure.',
       imageUrl: '/assets/projects/PolloLoco.png',
       link: 'https://stefan-horning.com/PolloLoco/index.html',
       technologies: ['Js', 'CSS', 'HTML'],
